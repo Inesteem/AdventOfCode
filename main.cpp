b5728477cb2a1e3635c9ffe96665dd76ab18d4b1
Lucifer KingOfHell <lucifer@hellfire.de>
2017-12-22 19:54:45 +0100

    got star 13

--- main.cpp
+++ main.cpp
@@ -6,20 +6,20 @@
 #include <fstream>
 #include <sstream>
 #include <unordered_map>
+#include <cassert>
 
-#define STAR9
+
+#define STAR8
 
 using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
-using std::unordered_map;
 using std::pair;
-using std::make_pair;
-
-unordered_map<int, vector<pair<int,vector<int> > > > database;
-
-void do_stuff(vector<int> &data);
+using std::make_pair; 
+using std::unordered_map;
+//prog name, (weight, num of references)
+unordered_map<string, pair<int,int> > programs;
 
 template<class T> void print(vector<T> vec){
     auto size = vec.size();
@@ -30,99 +30,56 @@ template<class T> void print(vector<T> vec){
 
 }
 
-template<class T> bool is_same(const vector<T> &vec, const vector<T> &vec2){
-    if(vec.size() != vec2.size()) return false;
-    for(int i = 0; i < vec.size(); ++i)
-        if(vec[i] != vec2[i]) return false;
-    return true;
-}
-
-int already_there(vector<int> &data, int cycle){
-    int sum = 0;
-    for (auto& n : data)
-        sum += n;
-    auto it = database.find(sum);
-    if(it == database.end()){
-        vector<pair<int,vector<int> > > vec = {make_pair(cycle,data)};        
-        database.insert(make_pair(sum,vec)); 
-        return 0;
+inline void insert(string &name, weight){
+    auto it = programs.find(name);
+    if(it == programs.end()){//no prog inside
+        programs.insert(make_pair(name,make_pair(weight,0)));
+        return;
     }
-    //for(auto &vec_it = (it->second).begin(); vec_it != (it->second).end(); ++vec_it){
-     //   if(is_same(*vec_it, data)) return true;
-    for(int i = 0; i < (it->second).size(); ++i){
-        if(is_same((it->second)[i].second, data)){
-             return cycle - (it->second)[i].first;
-        }
+    if(weight != -1){
+        it->second.first = weigth;
     }
-    (it->second).push_back(make_pair(cycle,data));
-    return 0;
+    ++(it->second.second);
 }
-
+inline void insert(string &name){
+    insert(name,-1);
+}
 
 int main(int argc, char *argv[]){
-    
-    int cnt = 0;
-    int cycle_diff;     
-    vector<int> data = {11, 11, 13, 7, 0, 15, 5, 5, 4, 4, 1, 1, 7, 1, 15, 11};
-    while(!(cycle_diff = already_there(data,cnt))){
-        do_stuff(data);
-        ++cnt;
+	if(argc != 2){
+		cout << "wrong parameter count, submit filename!" << endl;
+		exit(0);
+	}	
+	std::ifstream infile(argv[1], std::ifstream::in);
+	if(infile.fail()){
+		cout << "an error occured" << endl;
+		exit(-1);
+	}
+	std::string line;
+	int valid = 0;	
+	int lines = 0;	
+	while (std::getline(infile, line)) {
+    	std::istringstream iss(line);
+		string word = "";
+		vector<string> words;
+		while(iss >> word){
+			words.push_back(word);
+		}
+        if(!words.size()) continue;
+        assert(words.size() >= 2);
+       // words[1].erase(0,1);
+       // words[1].erase(str.size()-1,1);
+        insert(words[0],std::stoi(words[1],1));
+        cout << words[1] << " : " << std::stoi(words[1],1) << endl;
+        for(int i = 3; i < words.size(); ++i){
+                //words[i].erase(std::remove(words[i].begin(), words[i].end(), ','), words[i].end());
+            auto pos = words[i].size() - 1;
+            if(words[i][pos] == ',') words[i].erase(pos,1);
+            insert(words[i]);
+        }	
     }
-    cout << "Total: " << cnt << " cycles" << endl;
-    cout << "Diff : " << cycle_diff << " cycles" << endl;
-   	return 0;
-}
-
-void do_stuff(vector<int> &data){
-    cout << "---------------------" << endl;
-    print(data);
-    int start = max_element(data.begin(),data.end()) - data.begin();
-    int num = data[start];
-    int size = data.size();
-    int end = (start + num) % size;
-
-    if(!num) return;
-    if(start + num == end){
-        data[start] = 0;
-        for(int i = start + 1; i <=end; ++i){
-            ++data[i];
-        } 
-        print(data);
-        return;
-    } 
-
-    int to_add = num / size;
-    cout << "start " << start << endl;
-    cout << "end " << end << endl;
-    cout << "num " << num << endl;
-    cout << "to_add " << to_add << endl;
-
-    data[start] = 0;
-    if(start == end){
-        for(int i = 0; i <= size; ++i){ 
-           data[i] += to_add;
-        }
-
-    } else if(end <= start){
-        for(int i = 0; i <= end; ++i){ 
-           data[i] += to_add+1;
-        }
-        for(int i = end + 1; i <= start; ++i){ 
-           data[i] += to_add;
-        }
-        for(int i = start + 1; i < size; ++i){ 
-           data[i] += to_add + 1;
-        }
-    } else {
-        for(int i = 0; i < start + 1; ++i){ 
-           data[i] += to_add;
-        }
-        for(int i = start + 1; i <= end; ++i){ 
-           data[i] += to_add + 1;
-        }
-        for(int i = end + 1; i < size; ++i){ 
-           data[i] += to_add;
-        }
+    for(auto it : programs){
+        cout << it->first << " : " << it->second.first << " : " << it->second.second << endl;
     }
-    print(data);
+	return 0;
 }
