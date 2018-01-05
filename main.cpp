a36829f19c13bdab7a4f0e52a83effcd9a50b735
Lucifer Hemp <lucifer.hemp@fau.de>
2018-01-05 18:28:45 +0100

    i am stupid

--- main.cpp
+++ main.cpp
@@ -1,139 +1,137 @@
 #include "include/helper.h"
 
+enum Dir {DOWN = 1, UP = 2, LEFT = 4, RIGHT = 8};
+const char *DIRS[] =  {"DOWN", "UP  ", "LEFT", "RIGHT"};
+enum Way {BLANC = 0, VERTIC = 1, HORIZ = 2, CROSS = 4};
 
-#define INSTR    0
-#define REG_DST  1
-#define REG_SRC  2 
-#define OPERAND  2 
-queue<long> tasks_p1;
-queue<long> tasks_p2;
+struct Field{
+    int x,y;
+    int width, height;
+    vector<vector<int> > field;
+    int last_dir;
+    Field(vector<string> &lines):x(0),y(0),width(lines[0].size()),height(lines.size()), 
+            field(vector<vector<int> >(height)), last_dir(0xf){
+        x = lines[0].find("|");
+        assert(x != std::string::npos);
+        int width = lines[0].size();
 
-long snt_count = 0;
+        for(int i = 0; i < height; ++i){
+            assert(width == lines[i].size());
+            field[i] = vector<int>(width);
 
-unordered_map<string, long> reg_mem_p1;
-unordered_map<string, long> reg_mem_p2;
-
-void print_map(int id){
-    assert(id);
-    decltype(reg_mem_p1) &reg_mem =  (id == 1) ? reg_mem_p1 : reg_mem_p2 ;
-    //cout << "Program " << id << ":" << endl;
-    for(auto &reg : reg_mem){
-        cout << reg.first << ": " << reg.second << endl;
+            for(int j = 0; j < width; ++j){
+                if(     lines[i][j] == '|') field[i][j] = VERTIC; 
+                else if(lines[i][j] == '-') field[i][j] = HORIZ; 
+                else if(lines[i][j] == '+') field[i][j] = CROSS; 
+                else if(lines[i][j] == ' ') field[i][j] = BLANC; 
+                else field[i][j] = lines[i][j]; 
+            }
+        }
     }
-}
 
-int do_op(vector<string> & words, int id){
-    assert(id);
-    decltype(reg_mem_p1) &reg_mem =     (id == 1) ? reg_mem_p1 : reg_mem_p2 ;
-    decltype(tasks_p1)   &tasks =       (id == 1) ? tasks_p1   : tasks_p2 ;
-    decltype(tasks_p1)   &other_tasks = (id == 1) ? tasks_p2   : tasks_p1 ;
-    
-    bool is_digit = (words[words.size() - 1].find_first_not_of( "0123456789-" ) == string::npos);
+    inline bool check_dir(Dir dir){
+        return last_dir & dir;
+    }
 
-    if(words[INSTR] == "snd"){
-        if(is_digit)
-            other_tasks.push(stoi(words[1]));
-        else 
-            other_tasks.push(reg_mem[words[REG_DST]]);
-
-        if(id == 2) ++snt_count; 
-
-    } else if(words[INSTR] == "set"){
-        if(is_digit)
-            reg_mem[words[REG_DST]] = stoi(words[OPERAND]);
-        else 
-            reg_mem[words[REG_DST]] = reg_mem[words[REG_SRC]];
-
-    } else if(words[INSTR] == "add"){
-        if(is_digit)
-            reg_mem[words[REG_DST]] += stoi(words[OPERAND]);
-        else 
-            reg_mem[words[REG_DST]] += reg_mem[words[REG_SRC]];
-
-    } else if(words[INSTR] == "mul"){
-        if(is_digit)
-            reg_mem[words[REG_DST]] *= stoi(words[OPERAND]);
-        else 
-            reg_mem[words[REG_DST]] *= reg_mem[words[REG_SRC]];
-
-    } else if(words[INSTR] == "mod"){
-        if(is_digit)
-            reg_mem[words[REG_DST]] %= stoi(words[OPERAND]);
-        else 
-            reg_mem[words[REG_DST]] %= reg_mem[words[REG_SRC]];
+    inline void set_last_dir(Dir dir){
+        if(get_elem() == CROSS) last_dir = 0xf ^ dir;
+        else last_dir = dir; 
+    }
+    inline bool valid(){
+        return x > 0 && y > 0 && x < width && y < height;
+    }
  
-    } else if(words[INSTR] == "rcv"){
-//        if(!reg_mem[words[REG_DST]]) return 1;
-        if(tasks.empty()) return 0;
-        reg_mem[words[REG_DST]] = tasks.front();
-        tasks.pop();
- 
-    } else if(words[INSTR] == "jgz"){
-        bool is_cond_digit = (words[REG_DST].find_first_not_of( "0123456789-" ) == string::npos);
-        if(is_cond_digit){ 
-            if(stoi(words[REG_DST]) <= 0) return 1;
-        } else if(reg_mem[words[REG_DST]] <= 0) return 1;
-        int ret;
-        if(is_digit)
-            ret = stoi(words[OPERAND]);
-        else 
-            ret = reg_mem[words[REG_SRC]];
-        if(ret) return ret;
-    
-    } else { assert(false); }
+    Way skip_way(Way to_skip, int xoff, int yoff){
 
-    return 1;
-}
-        
+        if(!valid()) return BLANC;
+        while(get_elem() == to_skip){
+            x += xoff;
+            y += yoff;
+        } 
+        return (Way) get_elem();
+    }
+
+    bool new_dir(Dir ndir, Way fway, int xoff, int yoff){
+        if(!check_dir(ndir) ) return false;
+
+        int xold = x, yold = y;
+        x += xoff;
+        y += yoff;
+        auto nway = skip_way(fway, xoff, yoff);
+        if(nway == BLANC) {
+            x = xold;
+            y = yold;
+            return false;
+        }
+        if(field[yold][xold] != fway) field[yold][xold] = BLANC;
+        set_last_dir(ndir);
+        return true;
+    }
+
+
+    bool go_up(){
+
+        return new_dir(UP, HORIZ, 0, -1);
+    }
+    bool go_down(){
+
+        return new_dir(DOWN, HORIZ, 0, 1);
+    }
+
+    bool go_right(){
+
+        return new_dir(RIGHT, VERTIC, 1,0);
+    }
+
+    bool go_left(){
+
+        return new_dir(LEFT, VERTIC, -1, 0);
+    }
+
+    int get_elem(){
+        return field[y][x];
+    }
+    void print(){
+        cout << x << " , " << y << endl;
+        for(int h = 0; h < height; ++h){
+            for(int w = 0; w < width; ++w){
+                auto elem = field[h][w];    
+                if(h == y && w == x) cout << " #";
+                else if(!elem) cout << "  ";
+                else if(elem <= 4) cout << " " << elem;
+                else  cout << " " << (unsigned char) elem;
+            }
+            cout << endl;
+        }   
+        cout << endl;
+    
+    }
+};
+
 
 
 int main(int argc, char *argv[]){
-	if(argc != 2){
-		cout << "wrong parameter count, submit filename!" << endl;
-		exit(0);
-	}	
-    auto lines = get_lines(" ", argv[1], true);
-    int pos1 = 0; 
-    int pos2 = 0;
-    reg_mem_p1["p"] = 0;
-    reg_mem_p2["p"] = 1;
-    int p1_blocked = 0;
-    int p2_blocked = 0;
-    while(p1_blocked < 2 || p2_blocked < 2){
-        for(; pos1 < lines.size();){
-            cout << endl << "[1] ";
-            print(lines[pos1]);
-            auto ret = do_op(lines[pos1], 1);
-            if(!ret){
-                ++p1_blocked; 
-                cout << "P1 waiting at instruction["<<pos1<<"]: "; print(lines[pos1]); cout << endl;
-                cout << tasks_p2.size() << endl;
-                print_map(1);
-                break;
-            }
-            p1_blocked = 0;
-            pos1 += ret;
-            print_map(1);
-        }
-        for(; pos2 < lines.size();){
-   //         cout << endl << "[2] ";
-   //         print(lines[pos2]);
-            auto ret = do_op(lines[pos2], 2);
-            if(!ret){
-                ++p2_blocked; 
-                cout << "P2 waiting at instruction["<<pos2<<"]: "; print(lines[pos2]); cout << endl;
-                cout << tasks_p1.size() << endl;
-                print_map(2);
-                break;
-            }
-            p2_blocked = 0;
-            pos2 += ret;
-     //       print_map(2);
-        }
 
-        cout << "prog 1 send " << snt_count << " times a value!" << endl;
-        if((pos1 < 0 || pos1 >= lines.size()) && (pos2 < 0 || pos2 >= lines.size())) break;
+
+	if(argc != 2){
+        cout << "wrong parameter count, submit filename!" << endl;
+        exit(0);
     }
-    cout << "prog 1 send " << snt_count << " times a value!" << endl;
-	return 0;
+    string output;
+	auto lines = get_one_line("\n", argv[1], true);
+    Field field(lines);
+    string path = "";
+    unsigned char elem;
+    while(true){
+        field.print();
+        if((elem = (unsigned char) field.get_elem()) > 4) path += elem;
+        if(field.go_down()) continue;
+        if(field.go_left()) continue;
+        if(field.go_right()) continue;
+        if(field.go_up()) continue;
+        break;
+    }   
+    cout << "path " << path << endl;
+  
+   	return 0;
 }
