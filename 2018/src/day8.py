c6846ee73f27dd7205c7e40baee3113c0f597ea4
Lucifer <lucifer.hemp@fau.de>
2018-12-26 01:04:30 +0100

    got another star for day 8

---
+++ 2018/src/day8.py
@@ -0,0 +1,58 @@
+import re
+import sys
+import math
+import datetime 
+
+class Node():
+
+    def __init__(self):
+        self.childs = []
+        self.data= []
+  
+class Pair():
+
+    def __init__(self, idx, n_id):
+        self.idx = idx
+        self.n_id = n_id 
+
+
+
+def build_tree(nodes, n_id, n_info, idx):
+    this = n_id 
+    nodes.append(Node())
+    num_childs = n_info[idx]
+    num_data = n_info[idx+1]
+    idx += 2
+
+    for c in range(0, num_childs):
+        n_id += 1
+        nodes[this].childs.append(n_id)
+        p = build_tree(nodes, n_id, n_info, idx)
+        idx = p.idx 
+        n_id = p.n_id 
+        
+    for d in range(0, num_data):
+        nodes[this].data.append(n_info[idx])
+        idx += 1
+    
+    return Pair(idx, n_id)
+
+
+#n_info = [int(z) for z in [line.rstrip('\n') for line in open('../data/test.dat')][0].split(' ')]
+n_info = [int(z) for z in [line.rstrip('\n') for line in open('../data/day8.dat')][0].split(' ')]
+
+
+nodes = []
+
+build_tree(nodes, 0, n_info, 0)
+
+meta_data = 0
+
+for n in nodes:
+    for d in n.data:
+        meta_data += d
+    print(n.childs)
+    print(n.data)
+    print("-----")
+
+print("meta data : " + str(meta_data))
