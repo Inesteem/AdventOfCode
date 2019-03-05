1c81aa71fac59e9ecfe331a2dccdebdfbd3fddcd
Lucifer <lucifer.hemp@fau.de>
2019-03-05 20:11:21 +0300

    first part of day 14 solved

---
+++ 2018/src/day14.py
@@ -0,0 +1,36 @@
+
+import re
+import sys 
+
+scores = [3,7]
+pos_1 = 0
+pos_2 = 1
+
+
+if len(sys.argv) != 2:
+    print("usage: prog numIter")
+    exit(-1)
+
+while True:
+    new_score = scores[pos_1] + scores[pos_2]
+    if new_score > 9: #can never ne > 19
+        scores.append(1)
+        scores.append(new_score - 10)
+    else:
+        scores.append(new_score)
+    if len(scores) >= int(sys.argv[1]) + 10:
+        break
+    pos_1 = (1+pos_1+scores[pos_1]) % len(scores)
+    pos_2 = (1+pos_2+scores[pos_2]) % len(scores)
+            
+#print("whole sequence:")
+#for score in scores:
+#    sys.stdout.write(str(score) + " ")
+
+print()
+print("1. answer:")
+
+for i in range(int(sys.argv[1]), int(sys.argv[1]) + 10):
+    sys.stdout.write(str(scores[i]))
+
+print()
