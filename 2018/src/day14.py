a6246c73dd63544be24dcf6fff3ad19c4e026854
Lucifer <lucifer.hemp@fau.de>
2019-03-05 20:15:46 +0300

    first part of day 14 solved

--- 2018/src/day14.py
+++ 2018/src/day14.py
@@ -11,6 +11,12 @@ if len(sys.argv) != 2:
     print("usage: prog numIter")
     exit(-1)
 
+goal = [for x in sys.argv[1]: int(x)]
+print(goal)
+exit(0)
+
+
+
 while True:
     new_score = scores[pos_1] + scores[pos_2]
     if new_score > 9: #can never ne > 19
@@ -18,8 +24,11 @@ while True:
         scores.append(new_score - 10)
     else:
         scores.append(new_score)
-    if len(scores) >= int(sys.argv[1]) + 10:
-        break
+# break condition for star 1
+#    if len(scores) >= int(sys.argv[1]) + 10:
+#        break
+# break condition for star 2
+    if 
     pos_1 = (1+pos_1+scores[pos_1]) % len(scores)
     pos_2 = (1+pos_2+scores[pos_2]) % len(scores)
             
