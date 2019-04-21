9c8412207846d3d8ced2b93f210d28a755e93a52
Lucifer Hemp <lucifer.hemp@fau.de>
2019-04-22 00:04:48 +0200

    got star 2

--- 2016/rust/day07/src/main.rs
+++ 2016/rust/day07/src/main.rs
@@ -2,7 +2,75 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
-fn check_ip(s: &str) -> bool {
+
+fn check_bab(s: &str, c_o: &str, c_i: &str, i1: usize, i2: usize) -> bool{
+    if c_i == c_o{ 
+        return false;
+    }
+    //println!("{} ->  {}, {}", s,c_o,c_i); 
+
+    let tmp = s.split("").collect::<Vec<&str>>();
+    for i in i1+1..i2-2{
+        println!("--- {}:{}", i,tmp[i]);
+        if tmp[i] == c_o && tmp[i+2] == c_o && tmp[i+1] == c_i{
+            return true;
+        }
+    }
+    false 
+}
+
+
+fn check_ssl(s: &str) -> bool {
+    println!("{}", s);
+    let mut start = 0;
+
+    loop {
+        println!("--- {}", start);
+        let p1 = s[start..].find('[');
+        println!("{}", &s[start..]);
+        if p1 == None{
+            return false;
+        }
+        let i1 = start+p1.unwrap()+1;
+        let p2 = s[i1..].find(']');
+        println!("{}", &s[i1+1..]);
+        if p2 == None{
+            return false;
+        }
+        let i2 = i1+p2.unwrap()+1;
+        println!("{} .. {}", i1-1, i2-1);
+        start = i1;
+        let vec = s.split("").collect::<Vec<&str>>();
+
+        let mut in_bracks = false; 
+        for i in 1..vec.len()-2{
+            if vec[i] == "["{
+                in_bracks = true;
+                continue;
+            }
+            if vec[i] == "]"{
+                in_bracks = false;
+                continue;
+            }
+            if in_bracks {
+                continue;
+            }
+            if vec[i+1] == "[" || vec[i+2] == "["{
+                continue;
+            }
+            println!("- {}:{}", i,vec[i]);
+            if vec[i] == vec[i+2] && check_bab(s, vec[i+1], vec[i], i1, i2){
+                println!("found: {}{}{}",vec[i], vec[i+1], vec[i+2]); 
+                return true;
+            }
+        }
+        
+    }
+
+}
+
+
+fn check_tls(s: &str) -> bool {
     //let char_vec: Vec<char> = s.chars().collect();
     //for c in char_vec {
     //    println!("{}", c);
@@ -41,6 +109,8 @@ fn check_ip(s: &str) -> bool {
 }
 
 fn main() {
+
+
     let args: Vec<String> = env::args().collect();
 
     let filename = &args[1];
@@ -55,14 +125,19 @@ fn main() {
 
     let split_str = contents.split("\n");
 
-    let mut cnt = 0;
+    let mut tls_cnt = 0;
+    let mut ssl_cnt = 0;
 
     for s in split_str {
 
-        if check_ip(s) == true {
-            cnt += 1;
+        if check_tls(s) == true {
+            tls_cnt += 1;
+        }
+        if check_ssl(s) == true {
+            ssl_cnt += 1;
         }
         
     }
-    println!("{} IPs support TLS!", cnt );
+    println!("{} IPs support TLS!", tls_cnt );
+    println!("{} IPs support SSL!", ssl_cnt );
 }
