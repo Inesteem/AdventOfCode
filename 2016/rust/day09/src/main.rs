cdf2631c3f7172eb9fad5b3fdbcabd381a3bc091
Lucifer <lucifer.hemp@fau.de>
2019-04-23 16:44:18 +0200

    bad code for 2016::9::2, works but too slow

--- 2016/rust/day09/src/main.rs
+++ 2016/rust/day09/src/main.rs
@@ -14,6 +14,48 @@ fn length(s: &String)->i32{
 
 }
 
+fn get_len(s: &str)->i32{
+    let mut length = 0;
+    let mut start = 0;
+    loop {
+        if start >= s.len(){
+            return length;
+        }
+
+        let m1 = s[start..].find('(');
+        if m1 == None {
+            if start == 0 {
+                return s.len();
+            }
+            return length + s.len() - start;
+        } 
+
+        let p1 = m1.unwrap() + 1 + start;
+        if p1 > start+1 {
+            length += (p1 - 1 - start); 
+        }
+        let m2 = s[p1..].find(')');
+        if m2 == None {
+            assert!(false);
+        }
+        let p2 = m2.unwrap() + p1;
+        start = p2 + 1; 
+
+        let tmp = &s[p1..p2];
+        let args = tmp.split("x").collect::<Vec<&str>>();
+        let num_chars = args[0].parse::<usize>().unwrap(); 
+        let num_times = args[1].parse::<usize>().unwrap(); 
+        
+        length += get_len(s[start..start+num_chars]) * num_times; 
+
+        start += num_chars;
+             
+    }
+ 
+
+}
+
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
@@ -24,57 +66,9 @@ fn main() {
         .expect("something went wrong reading the file");
 
     let split_str = contents.split("\n");
-    for s1 in split_str {
-        let mut s = s1.clone();
-        println!("");
-        let mut start = 0;
-        let mut decomp = String::new();
-        let mut tmp_decomp;
-        loop {
-            if start >= s.len(){
-                start = 0;
-                tmp_decomp = decomp.clone();
-                s = &tmp_decomp;
-                decomp = String::new();
-            //    println!("str is now {}", s);
-                //break;
-            }
-
-            let m1 = s[start..].find('(');
-            if m1 == None {
-                decomp += &s[start..]; 
-                if start == 0{
-                    break;
-                }
-                start = s.len();
-                continue;
-            } 
-
-            let p1 = m1.unwrap() + 1 + start;
-            if p1 > start+1 {
-                decomp += &s[start..p1-1]; 
-            }
-            let m2 = s[p1..].find(')');
-            if m2 == None {
-                assert!(false);
-            }
-            let p2 = m2.unwrap() + p1;
-            start = p2 + 1; 
-
-            let tmp = &s[p1..p2];
-            let args = tmp.split("x").collect::<Vec<&str>>();
-            let num_chars = args[0].parse::<usize>().unwrap(); 
-            let num_times = args[1].parse::<usize>().unwrap(); 
-   
-            for _i in 0..num_times{
-                decomp += &s[start..start+num_chars]; 
-            }
-
-            start += num_chars;
-
-             
-        }
-        println!("length: {}", length(&decomp));
+    for s in split_str {
+         
+        println!("length: {}", get_len(s));
         //println!("decompressed string: {} , length: {}", decomp, length(&decomp));
 
     }
