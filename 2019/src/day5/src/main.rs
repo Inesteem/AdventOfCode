a9a2045f31e196555cabd9080ba0fec51fa97a13
Lucifer <lucifer.hemp@fau.de>
2020-01-05 17:39:23 +0100

    got first golden start for this

---
+++ 2019/src/day5/src/main.rs
@@ -0,0 +1,145 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::process;
+use std::io;
+
+
+
+
+
+struct Opcode{
+    instr : u8,
+    mode_1: bool,
+    mode_2: bool,
+    mode_3: bool 
+}
+
+impl Opcode {
+
+    fn new() -> Opcode {
+        Opcode{
+            instr: 0,
+            mode_1 : false,
+            mode_2 : false,
+            mode_3 : false
+        }
+    }
+
+    fn parse(&mut self, opcode : i32){
+        let digits: Vec<_> = opcode.to_string().chars().map(|d| d.to_digit(10).unwrap()).collect();
+        let len = digits.len();
+        self.instr = digits[len - 1] as u8;
+        if len >= 2 {
+            self.instr += (digits[len - 2] * 10) as u8;
+        } 
+        
+        if len >= 3 {
+            self.mode_1 = (digits[len-3] == 1);
+        } else { 
+            self.mode_1 = false;
+        }
+        
+        if len >= 4 {
+            self.mode_2 = (digits[len-4] == 1);
+        } else { 
+            self.mode_2 = false;
+        }
+         
+        
+        if len >= 5 {
+            self.mode_3 = (digits[len-5] == 1);
+        } else { 
+            self.mode_3 = false;
+        }
+       // println!("{:?} {} {} {} {}", digits, self.mode_3, self.mode_2, self.mode_1, self.instr);
+    }
+
+    fn do_op(&mut self, program: &mut Vec<i32>, i : usize, ops: & Vec<Box<dyn Fn(&mut Vec<i32>,i32,i32,i32) -> usize > > ) -> usize {
+      self.parse(program[i]);
+      //println!("{} {} {} {}", i , program[i+1], program[i+2], program[i+3]);
+      let mut o1 = program[i+1];
+      let mut o2 :i32 = 0;
+      let mut o3 :i32 = 0;
+
+      if self.instr < 3 {
+          if !self.mode_1 {
+              o1 = program[o1 as usize];
+          }  
+
+
+          o2 = program[i+2];
+          if !self.mode_2 {
+              o2 = program[o2 as usize];
+          }  
+
+          o3 = program[i+3];
+//          if !self.mode_3 {
+//              o3 = program[o3 as usize];
+//          }  
+      }
+      //println!("{} {} {}", o1, o2, o3);
+      return ops[(self.instr - 1) as usize] (program, o1, o2, o3) + i;
+    }
+}
+
+fn read_in_int() -> i32 {
+    let mut input_text = String::new();
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("failed to read from stdin");
+
+    let trimmed = input_text.trim();
+    match trimmed.parse::<i32>() {
+        Ok(i) => return i,
+        Err(..) => println!("this was not an integer: {}", trimmed),
+    };
+    return -1
+}
+
+fn read_inputs(filename : String) -> std::io::Result<String> {
+    let file = File::open(filename)?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    contents.pop();
+    println!("{}", &contents);
+    Ok(contents)
+}
+
+
+fn main() {
+    let mut opc = Opcode::new();
+
+    let ops : Vec<Box<dyn Fn(&mut Vec<i32>,i32,i32,i32) -> usize> > = vec![
+        Box::new(|v,o1,o2,o3|{ v[o3 as usize] = o1 + o2; return 4}),
+        Box::new(|v,o1,o2,o3|{ v[o3 as usize] = o1 * o2; return 4}),
+        Box::new(|v,o1,_,_|  { print!("input  : "); io::stdout().flush().unwrap(); v[o1 as usize] = read_in_int(); return 2}),
+        Box::new(|v,o1,_,_|  { println!("output : {}", v[o1 as usize]); return 2})
+    ];
+
+
+    let mut input_vec : Vec<i32>;
+    match read_inputs("../../data/day5.txt".to_string()) {
+        Ok(inputs) =>  
+            //let input_vec : Vec<u32> = inputs.split(",")
+            input_vec = inputs.split(",")
+            .map(|x| x.parse().expect("Not an integer!"))
+            .collect(),
+        Err(_) => process::exit(0),
+            
+    }
+
+    let mut program = input_vec;
+    let mut i : usize = 0;
+    while i < program.len() {
+        
+        if  program[i] == 99  {
+            break;
+        }
+
+        i = opc.do_op(&mut program, i, & ops);
+
+    }
+
+}
