e94dd0d3d4de32aaaee82694b9c2df08231ccb8b
Lucifer <lucifer.hemp@fau.de>
2020-12-12 12:37:23 +0100

    got the second star of day8

--- 2020/day8/main.rs
+++ 2020/day8/main.rs
@@ -26,12 +26,39 @@ fn main() {
         }
     }
 
+    let (_,acc) = check_end(&mut operations);
+    println!("Star1: {}", acc);
+
+    for i in 0..operations.len() {
+        let (comm_b, num_b) = &operations[i];
+        let comm = comm_b.to_string();
+        let num = *num_b;
+
+        if comm.eq("nop") {
+            operations[i] = ("jmp".to_string(),num);
+        } else if comm.eq("jmp"){
+            operations[i] = ("nop".to_string(),num);
+        }
+
+        let (fin,acc) = check_end(&mut operations);
+        if fin {
+            println!("Star2: {}",acc);
+            break;
+        }
+        
+        operations[i] = (comm.to_string(),num);
+    }
+
+}
+
+fn check_end(operations : &mut Vec<(String,i32)>) -> (bool, i32){
     let mut i : usize = 0;
     let mut acc : i32 = 0;
     let mut seen = vec![false; operations.len()];
     loop {
+        if i >= operations.len()  { return (true,acc); }
         let (comm, num) = &operations[i];
-        println!("{} {}", &comm, &num);
+        //println!("{} {}", &comm, &num);
         if seen[i] { break; }
         seen[i] = true;
         match comm.as_str() {
@@ -41,6 +68,5 @@ fn main() {
             _ => (),
         }
     }
-    println!("{}", acc);
-
+    (false, acc)
 }
