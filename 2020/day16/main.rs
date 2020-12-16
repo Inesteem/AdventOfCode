8db714f2be9528ccbb42246bb234355c24a89e5c
lucifer <lucifer.hemp@fau.de>
2020-12-16 11:14:14 +0100

    got second star

--- 2020/day16/main.rs
+++ 2020/day16/main.rs
@@ -1,5 +1,124 @@
 use lib::io::*;
 
+#[derive(Debug)]
+struct Range {
+    start : usize,
+    end : usize,
+}
+impl Range {
+    fn new(i : &str) -> Self {
+        let intervals : Vec<&str> = i.split('-').collect();
+        let start = intervals[0].parse::<usize>().unwrap();
+        let end = intervals[1].parse::<usize>().unwrap();
+        Self {
+            start : start,
+            end : end,
+        }
+    }
+    fn in_range(&self, val : usize) -> bool {
+        return val >= self.start && val <= self.end;
+    }
+}
+
+#[derive(Debug)]
+struct Field {
+    name : String,
+    ranges : Vec<Range>,
+}
+
+impl Field {
+    
+    fn new(line : &Vec<String>) -> Self{
+        Self {
+            ranges : vec![Range::new(&line[1]),Range::new(&line[2])],
+            name : line[0].clone(),
+        }
+    }
+    fn in_range(&self, val : usize)->bool {
+        for r in &self.ranges {
+            if r.in_range(val) {
+                return true;
+            }
+        }
+        false  
+    }
+}
+
+fn remove_from(needle : &str, haystack : &mut Vec<Vec<&str>>){
+    for l in haystack {
+        if l.len() <= 1 { continue; }
+        l.retain(|&x| !x.eq(needle));
+        //let index = l.iter().position(|x| (*x).eq(needle)).unwrap();
+        //l.remove(index);
+    }
+}
+
+
+
+fn star2(list : &mut Vec<Vec<String>>){
+
+    let mut fields = vec![];
+    let mut yt_idx = 0;
+    while !list[yt_idx][0].eq("-") {
+        fields.push(Field::new(&list[yt_idx]));
+        println!("{:?}\n", fields[fields.len()-1]);
+        yt_idx += 1;
+    }
+
+    yt_idx += 2;
+
+    println!("{} / {}",yt_idx,list.len());
+    let mut possible_fields : Vec<Vec<&str>> = vec![];
+    
+    for col in 0..fields.len() {
+        possible_fields.push(Vec::new());
+        println!("");
+        for f in 0..fields.len(){
+
+            let mut valid = true;
+            let field = &fields[f]; 
+            
+            for row in yt_idx..list.len() {
+                if !field.in_range(list[row][col].parse::<usize>().expect("not a number")) {
+                  //  println!("{} not in range for {}", list[row][col], field.name);
+                    valid = false;
+                    break;
+                } else {
+                //    println!("{} in range for {}", list[row][col], field.name);
+                }
+            }
+            if valid {
+                possible_fields[col].push(&field.name[..]);
+            }
+        }
+        //println!("{:?}",possible_fields[col]);
+    }
+
+        println!("---");
+
+    for _i in 0..100 {
+        for i in 0..possible_fields.len() {
+            if possible_fields[i].len() == 1 {
+                remove_from(&possible_fields[i][0], &mut possible_fields);
+            }
+        }
+    }
+
+    for f in 0..possible_fields.len() {
+        println!("{:?}", possible_fields[f]);
+    }
+    let mut mul = 1;
+    for f in 0..possible_fields.len() {
+        if possible_fields[f][0].len() >= "departure".len() &&  possible_fields[f][0][0.."departure".len()].eq("departure") {
+            println!("{} {}", f,possible_fields[f][0]);
+            mul *= list[yt_idx-1][f].parse::<usize>().unwrap();
+            println!("{}",list[yt_idx-1][f].parse::<usize>().unwrap());
+        }
+
+    }
+    println!("star2: {}", mul);
+}
+
 fn add_range(l : &str,ranges : &mut Vec<bool>) {
     let intervals : Vec<&str> = l.split('-').collect();
     println!("{:?}",intervals);
@@ -13,10 +132,10 @@ fn add_range(l : &str,ranges : &mut Vec<bool>) {
     }
 }
 
-fn star1(){
+fn star1()-> Vec<Vec<String>>{
     let mut ranges = vec![];
     let mut line = "".to_string();
-
+    let mut list = vec![];
     loop {
         line = read_line_from_stdin().unwrap().trim().to_string();
         println!("{}",line);
@@ -28,26 +147,40 @@ fn star1(){
         println!("- {:?}", split2);
         add_range(split2[1], &mut ranges);
         add_range(split2[3], &mut ranges);
+        list.push(vec![split1[0].to_string(), split2[1].to_string(), split2[3].to_string()]);
+    }
+    list.push(vec!["-".to_string()]);
+
+    loop {
+        line = read_line_from_stdin().unwrap().trim().to_string();
+        println!("{}",line);
+        if line.len() == 0 { continue; }
+        if line.eq("nearby tickets:") { break; }
+        let split: Vec<String> = line.split(',').map(|x| x.to_string()).collect();
+        list.push(split);
     }
 
-
-    while !line.eq("nearby tickets:\n"){
-        line = read_line_from_stdin().unwrap();
-    }
     let mut sum = 0;
     loop {
         line = read_line_from_stdin().unwrap().trim().to_string();
         println!("{}",line);
         if line.len() == 0 {break; }
         let splitted : Vec<usize> = line.split(',').map(|x| x.parse().unwrap()).collect();
+        let mut sane = true;
         for s in splitted {
-            if s >= ranges.len() || !ranges[s] { sum += s; }
+            if s >= ranges.len() || !ranges[s] { sum += s; sane = false; }
+        }
+        if sane {
+        list.push(line.split(',').map(|x| x.to_string()).collect());
         }
     }
     //println!("{:?}", ranges);
     println!("star1: {}", sum);
+    list
 }
 
 fn main() {
-    star1();
+    let mut list = star1();
+    println!("{:?}\n\n", list);
+    star2(&mut list);
 }
