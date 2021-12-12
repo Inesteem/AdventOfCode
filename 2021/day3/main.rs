cf13fc2c4669fbba3b32d8c28cb28ffecd1d2cf7
Lucifer Hemp <lucifer.hemp@fau.de>
2021-12-12 10:16:11 +0100

    save

--- 2021/day3/main.rs
+++ 2021/day3/main.rs
@@ -14,26 +14,12 @@ fn read_inputs(filename : String) -> std::io::Result<String> {
 }
 
 
-fn main() {
-    let slidingWindowSize = 3;
-    let mut numbers: Vec<u32>;
-    match read_inputs("data".to_string()) {
-        Ok(inputs) =>  
-            //let input_vec : Vec<u32> = inputs.split(",")
-            numbers = inputs.split("\n")
-            .map(|x| u32::from_str_radix(x, 2).unwrap())
-            .collect(),
-        Err(_) => process::exit(0),
-    }
-
-    let bits : u32 = 12;
-    let mut zeroes = vec![0; bits as usize];
-    let mut ones = vec![0; bits as usize];
-
+fn frequencies(numbers : &Vec<u32>, bits : u32, ones : &mut Vec<u32>, zeroes : &mut Vec<u32>)
+{
     for i in 0..numbers.len() {
         let mut num = numbers[i];
         for b in 0..bits {
-            if (num & 0x1 == 0x1) {
+            if (num & 0x1) == 0x1 {
                 zeroes[(bits-b-1) as usize] += 1;
             } else {
                 ones[(bits-b-1) as usize] += 1;
@@ -41,7 +27,17 @@ fn main() {
             num >>= 1;
         }
     }
-    
+
+}
+
+
+fn star1(numbers : &Vec<u32>, bits : u32) {
+
+    //calculate frequencies
+    let mut zeroes = vec![0 as u32; bits as usize];
+    let mut ones = vec![0 as u32; bits as usize];
+    frequencies(numbers, bits, &mut zeroes, &mut ones);
+
     let mut gamma : u32 = 0;
     for b in 0..bits {
         gamma <<= 1; 
@@ -52,7 +48,95 @@ fn main() {
 
     let epsilon = gamma ^ ((0x1 << bits) - 1);
 
-//   println!("{} {}", gamma, epsilon);
-   println!("{}", gamma *  epsilon);
-    
+    println!("{}", gamma * epsilon);
+
+}
+
+//sort 1 at the start and 0 at the end
+fn order(numbers : &mut [u32], bit : u32) ->  usize {
+    let mask = 0x1 << bit;
+
+    let mut start = 0;
+    let mut end = numbers.len() - 1;
+
+    loop {
+        while start != end {
+            if (numbers[start] & mask) == 0 {
+                break;
+            }
+            start += 1;
+        }
+        while start != end {
+            if (numbers[end] & mask) != 0 {
+                break;
+            }
+            end -= 1;
+        }
+
+        if start == end {
+            break;
+        }
+
+        let tmp = numbers[start];
+        numbers[start] = numbers[end];
+        numbers[end] = tmp;
+    }
+
+    start
+}
+// assumption: each col hast at least 1 zero and 1 one
+// (as proven by known input)
+fn oxy(bits : u32, numbers : &mut Vec<u32>) -> u32{
+
+
+    let mut start : usize= 0;
+    let mut end : usize = numbers.len();
+    let mut mid : usize = 0;
+
+    for b in (0..bits).rev() {
+        println!("{} {} {}", start, end, mid);
+        if(start - end <= 1) {
+            return numbers[start];
+        }
+        mid = order(&mut numbers[start..end], b);
+        // more zeroes
+        if mid < ((end - start) / 2) as usize {
+            start = mid + 1;
+            println!("0");
+        }
+        else 
+        {
+            end = mid + 1;
+            println!("1");
+        }
+
+        for i in start..end {
+            println!("{:05b}", numbers[i]);
+        }
+
+
+    }
+    println!("");
+    numbers[mid]
+
+}
+
+fn main() {
+    let slidingWindowSize = 3;
+    let mut numbers: Vec<u32>;
+    match read_inputs("test".to_string()) {
+        Ok(inputs) =>  
+            //let input_vec : Vec<u32> = inputs.split(",")
+            numbers = inputs.split("\n")
+            .map(|x| u32::from_str_radix(x, 2).unwrap())
+            .collect(),
+        Err(_) => process::exit(0),
+    }
+
+    //let bits : u32 = 12;
+    let bits : u32 = 5;
+
+
+
+    println!("\n\n {}", oxy(bits, &mut numbers));
 }
