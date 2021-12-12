8b5ef9c850afeea43d48ae41e90c1128a0f3c6dc
Lucifer Hemp <lucifer.hemp@fau.de>
2021-12-12 10:59:28 +0100

    Got star 2

--- 2021/day3/main.rs
+++ 2021/day3/main.rs
@@ -52,79 +52,53 @@ fn star1(numbers : &Vec<u32>, bits : u32) {
 
 }
 
-//sort 1 at the start and 0 at the end
-fn order(numbers : &mut [u32], bit : u32) ->  usize {
-    let mask = 0x1 << bit;
+fn co2Cmp(a : usize, b :usize) -> bool {
+    a <= b
+}
+
+
+fn oxyCmp(a : usize, b :usize) -> bool {
+    a > b
+}
+
+fn getValue(bits : u32, numbers : &mut Vec<u32>, cmp : fn(a:usize,b:usize) -> bool) -> u32{
 
     let mut start = 0;
-    let mut end = numbers.len() - 1;
+    let mut end = numbers.len();
 
-    loop {
-        while start != end {
-            if (numbers[start] & mask) == 0 {
+    for b in (1..=bits).rev() {
+        if end - start == 1 {
+          break;  
+        }
+
+        let mask = (0x1 << b)-1;
+        let bit = 0x1 << (b-1);
+
+        numbers[start..end].sort_by(|a,b| (a & mask).cmp(&(b & mask)));
+
+        let mid : usize = (end + start)/2;
+        for n in start..end {
+            if numbers[n] & bit != 0 {
+                //less zeroes
+                if cmp(n,mid)
+                {
+                    end = n;
+                } else {
+                    start = n;
+                }
                 break;
             }
-            start += 1;
         }
-        while start != end {
-            if (numbers[end] & mask) != 0 {
-                break;
-            }
-            end -= 1;
-        }
-
-        if start == end {
-            break;
-        }
-
-        let tmp = numbers[start];
-        numbers[start] = numbers[end];
-        numbers[end] = tmp;
     }
 
-    start
-}
-// assumption: each col hast at least 1 zero and 1 one
-// (as proven by known input)
-fn oxy(bits : u32, numbers : &mut Vec<u32>) -> u32{
-
-
-    let mut start : usize= 0;
-    let mut end : usize = numbers.len();
-    let mut mid : usize = 0;
-
-    for b in (0..bits).rev() {
-        println!("{} {} {}", start, end, mid);
-        if(start - end <= 1) {
-            return numbers[start];
-        }
-        mid = order(&mut numbers[start..end], b);
-        // more zeroes
-        if mid < ((end - start) / 2) as usize {
-            start = mid + 1;
-            println!("0");
-        }
-        else 
-        {
-            end = mid + 1;
-            println!("1");
-        }
-
-        for i in start..end {
-            println!("{:05b}", numbers[i]);
-        }
-
-
-    }
-    println!("");
-    numbers[mid]
+    numbers[start]
 
 }
 
 fn main() {
     let slidingWindowSize = 3;
     let mut numbers: Vec<u32>;
-    match read_inputs("test".to_string()) {
+    match read_inputs("data".to_string()) {
         Ok(inputs) =>  
             //let input_vec : Vec<u32> = inputs.split(",")
             numbers = inputs.split("\n")
@@ -133,10 +107,9 @@ fn main() {
         Err(_) => process::exit(0),
     }
 
-    //let bits : u32 = 12;
-    let bits : u32 = 5;
+    let bits : u32 = 12;
+    //let bits : u32 = 5;
 
 
-
-    println!("\n\n {}", oxy(bits, &mut numbers));
+    println!("\n\n {}", getValue(bits, &mut numbers, oxyCmp) * getValue(bits, &mut numbers, co2Cmp));
 }
