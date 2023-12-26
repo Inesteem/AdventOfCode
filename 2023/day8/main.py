914dbe41f15899460268890ba4cec39de607095e
Judith Hemp <lucifer.hemp@fau.de>
2023-12-26 05:07:30 -0600

    day8 star2

--- 2023/day8/main.py
+++ 2023/day8/main.py
@@ -3,6 +3,7 @@
 from collections import defaultdict
 from dataclasses import dataclass
 import re
+import sys
 
 @dataclass
 class Edge:
@@ -19,7 +20,7 @@ class Edge:
 def steps(edges, instructions, start ="AAA", end="ZZZ"):
   node = start
   i = 0
-  while node != end:
+  while re.match(end, node) == None:
     d = instructions[i%len(instructions)]
     if d == "L":
       node = edges[node].left
@@ -28,9 +29,26 @@ def steps(edges, instructions, start ="AAA", end="ZZZ"):
     i += 1
   return i
 
+def gcd(x, y):
+
+   while(y):
+       x, y = y, x % y
+   return x
+
+def _lcm(x, y):
+   lcm = (x*y)//gcd(x,y)
+   return lcm
+
+def lcm(args : list[int]):
+  if len(args) == 1:
+    return args[0]
+  res = _lcm(args[0], args[1])
+  for arg in args[2:]:
+    res = _lcm(res, arg)
+  return res
 
 def main():
-  lines = [l.strip() for l in open("input", "r").readlines()]
+  lines = [l.strip() for l in open(sys.argv[1], "r").readlines()]
 
   instructions = lines[0]
 
@@ -48,19 +66,24 @@ def main():
       end_nodes.add(s[0])
 
   print(start_nodes, end_nodes)
-  print("star1", steps(edges, instructions))
-
-  nodes = [n for n in start_nodes]
-  step=0
-  while not all([n in end_nodes for n in nodes]):
-    d = instructions[step%len(instructions)]
-    for i in range(len(nodes)):
-      if d == "L":
-        nodes[i] = edges[nodes[i]].left
-      else:
-        nodes[i] = edges[nodes[i]].right
-    step += 1
-  print("star2",step)
+  #print("star1", steps(edges, instructions))
+  
+  node_steps = []
+  for node in start_nodes:
+    node_steps += [steps(edges, instructions, node, "..Z")]
+  print(lcm(node_steps))
+ # nodes = [n for n in start_nodes]
+ # step=0
+ # while not all([n in end_nodes for n in nodes]):
+ #   d = instructions[step%len(instructions)]
+ #   for i in range(len(nodes)):
+ #     if d == "L":
+ #       nodes[i] = edges[nodes[i]].left
+ #     else:
+ #       nodes[i] = edges[nodes[i]].right
+ #   step += 1
+ #   print(step)
+ # print("star2",step)
 
 
 if __name__ == "__main__":
